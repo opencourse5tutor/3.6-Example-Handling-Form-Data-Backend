e952c53847514c42887a79ff94f3ed2f60618d5f
Piumi Perera <piumidp96@gmail.com>
2022-04-12 15:24:27 +0530

    Added database connection

---
+++ database.js
@@ -0,0 +1,38 @@
+var sqlite3 = require("sqlite3").verbose();
+var md5 = require("md5");
+
+const DBSOURCE = "db.sqlite";
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+  if (err) {
+    // Cannot open database
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log("Connected to the SQlite database.");
+    db.run(
+      `CREATE TABLE products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            productName text, 
+            description text,
+            unitPrice INTEGER
+            )`,
+      (err) => {
+        if (err) {
+          // Table already created
+        } else {
+          // Table just created, creating some rows
+          var insert =
+            "INSERT INTO products (productName, description, unitPrice) VALUES (?,?,?)";
+          db.run(insert, [
+            "White Basmathi Rice",
+            "White Basmathi Rice imported from Pakistan. High-quality rice with extra fragrance. Organically grown.",
+            200,
+          ]);
+        }
+      }
+    );
+  }
+});
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require("express");
+var db = require("./database.js");
+var md5 = require("md5");
 var app = express();
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-var HTTP_PORT = 8000;
 const cors = require("cors");
 app.use(
   cors({
@@ -16,78 +17,53 @@ app.use(
 );
 
 // Start server
+var HTTP_PORT = 8000;
+
 app.listen(HTTP_PORT, () => {
   console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
 });
 
-//Product Data
-var products = [
-  {
-    id: "001",
-    productName: "White Basmathi Rice",
-    unitPrice: "400",
-    description:
-      "White Basmathi Rice imported from pakistan. High quality rice with extra fragnance. Organically grown.",
-  },
-  {
-    id: "002",
-    productName: "Sugar",
-    unitPrice: "200",
-    description: "White sugar manufactured by Palwatte Factory",
-  },
-
-  {
-    id: "003",
-    productName: "Flour",
-    unitPrice: "190",
-    description: "Super fine whole grain general purpose flour",
-  },
-  {
-    id: "004",
-    productName: "Dhal",
-    unitPrice: "200",
-    description: "Imported mysoor dhal from India",
-  },
-];
-
 //HTTP GET method
 app.get("/api/products", (req, res, next) => {
-  try{
-    res.status(200).json({
-      message: "success",
-      data: products,
+  try {
+    var sql = "select * from products";
+    var params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.status(200).json({
+        message: "success",
+        data: rows,
+      });
     });
-  }
-  catch(E){
+  } catch (E) {
     res.status(400).send(E);
   }
 });
 
 //HTTP POST method
 app.post("/api/products", (req, res, next) => {
-    try{
-         const {
-           productName,
-           description,
-           unitPrice,
-         } = req.body;
+  try {
+    const { productName, description, unitPrice } = req.body;
 
-         var lastId = products.length + 1;
-         var newProduct = {
-             id: "00" + lastId,
-             productName: productName,
-             unitPrice: unitPrice,
-             description: description 
-         };
-         products.push(newProduct);
-
-         res.status(200).json({
-           message: "success",
-           data: newProduct,
-           id: newProduct.id
-         });
-    }
-    catch(E){
-        res.status(400).send(E); 
-    }
+    var sql =
+      "INSERT INTO products (productName, description, unitPrice) VALUES (?,?,?)";
+    var params = [productName, description, unitPrice];
+    db.run(sql, params, function (err, result) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      } else {
+        res.status(200).json({
+          message: "success",
+          data: req.body,
+          id: this.lastID,
+        });
+      }
+    });
+  } catch (E) {
+    res.status(400).send(E);
+  }
 });
